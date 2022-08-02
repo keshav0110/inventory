47fdec2a318da023ae1fbb3ebaa42b89bc59b7dc
Keshav <keshavmalhotra017@gmail.com>
2022-08-02 09:58:50 +0530

    IMS Version-1.0

---
+++ inventory/src/main/java/com/coforge/training/ims/service/ProductService.java
@@ -0,0 +1,38 @@
+package com.coforge.training.ims.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.coforge.training.ims.model.Product;
+import com.coforge.training.ims.repository.ProductRepository;
+
+@Service
+@Transactional
+public class ProductService {
+
+    @Autowired
+    private ProductRepository prepo;
+
+    public void saveProduct(Product p) { //User defined method in service class.
+
+        prepo.save(p); //Invokes save() method defined in JPA repo.    
+    }
+
+
+    public List<Product> listAll(){
+
+        return prepo.findAll(); //Define in JPA repo.
+    }
+
+    public Product get(long id) {
+        return prepo.findById(id).get();  // defined in JPA repo
+    }
+
+
+    public void delete(long id) {
+        prepo.deleteById(id);  // defined in JPA repo
+    }
+}
